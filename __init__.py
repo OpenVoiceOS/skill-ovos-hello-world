f69b46a5b05805ee832f43d8fb08fde63f766819
KMahoney7 <123687554+KMahoney7@users.noreply.github.com>
2024-02-16 12:49:20 -0800

    Update __init__.py for NFLTeams

--- __init__.py
+++ __init__.py
@@ -82,7 +82,12 @@ class AlexIsFatTest(OVOSSkill):
     def handle_thank_you_intent(self, message):
         """This is an Adapt intent handler, it is triggered by a keyword."""
         self.speak_dialog("alex.is.fat")
- 
+
+    @intent_handler(IntentBuilder("NFLTeamsIntent").require("NFLTeams"))
+    def handle_thank_you_intent(self, message):
+        """This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("nfl.teams")
+    
     @intent_handler(IntentBuilder("PrincessPeaIntent").require("PrincessPea"))
     def handle_thank_you_intent(self, message):
         """This is an Adapt intent handler, it is triggered by a keyword."""
