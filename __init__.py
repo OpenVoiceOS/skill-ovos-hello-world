2e28b20f891ac1506fc0f8f66974a095bf1f38de
Ddp423 <125301716+Ddp423@users.noreply.github.com>
2024-02-16 12:41:34 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -88,6 +88,11 @@ class AlexIsFatTest(OVOSSkill):
         """This is an Adapt intent handler, it is triggered by a keyword."""
         self.speak_dialog("princess.pea")
 
+    @intent_handler(IntentBuilder("FavSubnautCreatureIntent").require("FavSubnautCreature"))
+    def handle_thank_you_intent(self, message):
+        """This is an Adapt intent handler, it is triggered by a keyword."""
+        self.speak_dialog("fav.subnaut.creature")
+    
     @intent_handler("HowAreYou.intent")
     def handle_how_are_you_intent(self, message):
         """This is a Padatious intent handler.
